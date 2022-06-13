aa47b88608cbba9e56478c82881ccdba17924c6e
benjyiw <ben@wese.io>
2022-06-13 17:03:30 -0600

    feat: init codeartifact repository

---
+++ main.tf
@@ -0,0 +1,153 @@
+# create optional domain kms encryption key
+locals {
+  kms_key_description = (
+    var.codeartifact_domain_kms_key_description == "" ?
+    "domain kms key for ${var.codeartifact_domain_name} domain" :
+    var.codeartifact_domain_kms_key_description
+  )
+}
+
+resource "aws_kms_key" "codeartifact_domain" {
+  count       = var.enable_codeartifact_domain_kms_key ? 1 : 0
+  description = local.kms_key_description
+  tags        = var.tags
+}
+
+# create codeartifact domain
+locals {
+  encryption_key_arn = (
+    var.enable_codeartifact_domain_kms_key ?
+    one(aws_kms_key.codeartifact_domain[*].arn) :
+    var.codeartifact_domain_kms_key_arn
+  )
+}
+
+resource "aws_codeartifact_domain" "domain" {
+  count          = var.enable_codeartifact_domain ? 1 : 0
+  domain         = var.codeartifact_domain_name
+  encryption_key = local.encryption_key_arn
+  tags           = var.tags
+}
+
+# create repositories
+locals {
+  domain = (
+    var.enable_codeartifact_domain ?
+    one(aws_codeartifact_domain.domain[*].domain) :
+    var.codeartifact_domain_name
+  )
+}
+
+resource "aws_codeartifact_repository" "repository" {
+  count       = length(var.codeartifact_repositories)
+  repository  = var.codeartifact_repositories[count.index].name
+  description = var.codeartifact_repositories[count.index].description
+  domain      = local.domain
+  tags        = var.tags
+
+  dynamic "external_connections" {
+    for_each = var.codeartifact_repositories[count.index].external_connections
+
+    content {
+      external_connection_name = external_connections.value.external_connection_name
+    }
+  }
+}
+
+# CICD role with access to codeartifact and ecr
+data "aws_iam_policy_document" "cicd_codeartifact" {
+  count = var.enable_cicd_role ? 1 : 0
+
+  statement {
+    actions = [
+      "codeartifact:GetAuthorizationToken",
+      "codeartifact:GetRepositoryEndpoint",
+      "codeartifact:PublishPackageVersion",
+      "codeartifact:ReadFromRepository",
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "cicd_codeartifact_sts" {
+  count = var.enable_cicd_role ? 1 : 0
+
+  statement {
+    actions = [
+      "sts:GetServiceBearerToken",
+    ]
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:AWSServiceName"
+      values = [
+        "codeartifact.amazonaws.com",
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "cicd_ecr" {
+  count = var.enable_cicd_role ? 1 : 0
+
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:InitiateLayerUpload",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "cicd_assume_role_policy" {
+  count = var.enable_cicd_role ? 1 : 0
+
+  statement {
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = var.cicd_role_trust_arns
+    }
+  }
+}
+
+resource "aws_iam_role" "cicd_role" {
+  count = var.enable_cicd_role ? 1 : 0
+
+  name               = var.cicd_role_name
+  description        = var.cicd_role_description
+  assume_role_policy = data.aws_iam_policy_document.cicd_assume_role_policy[count.index].json
+  tags               = var.tags
+
+  inline_policy {
+    name   = "cicd-code-artifact"
+    policy = data.aws_iam_policy_document.cicd_codeartifact[count.index].json
+  }
+
+  inline_policy {
+    name   = "cicd-code-artifact-sts"
+    policy = data.aws_iam_policy_document.cicd_codeartifact_sts[count.index].json
+  }
+
+  inline_policy {
+    name   = "cicd-ecr"
+    policy = data.aws_iam_policy_document.cicd_ecr[count.index].json
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,77 @@
+variable "enable_codeartifact_domain_kms_key" {
+  description = "Whether to enable creation of a KMS key for the CodeArtifact domain"
+  type        = bool
+  default     = false
+}
+
+variable "codeartifact_domain_kms_key_description" {
+  description = "Description of KMS key to create if enabled"
+  type        = string
+  default     = ""
+}
+
+variable "enable_codeartifact_domain" {
+  description = "Whether to enable creation of a CodeArtifact domain"
+  type        = bool
+  default     = true
+}
+
+variable "codeartifact_domain_name" {
+  description = "Name of CodeArtifact domain to create or reference based on var.enable_codeartifact_domain"
+  type        = string
+}
+
+variable "codeartifact_domain_kms_key_arn" {
+  description = "CodeArtifact domain KMS key to use if var.enable_codeartifact_domain_kms_key is disabled"
+  type        = string
+  default     = null
+}
+
+variable "codeartifact_repositories" {
+  description = "List of repositories to create. Defaults to NPM with public NPM external connection"
+  type = list(object({
+    name        = string
+    description = string
+    external_connections = list(object({
+      external_connection_name = string
+    }))
+  }))
+  default = [
+    {
+      name        = "npm"
+      description = "NPM repository"
+      external_connections = [{
+        external_connection_name = "public:npmjs"
+      }]
+    }
+  ]
+}
+
+variable "enable_cicd_role" {
+  description = "Enables creation of a CICD role which grants access to Code Artifact and ECR"
+  type        = bool
+  default     = false
+}
+
+variable "cicd_role_trust_arns" {
+  description = "ARNs to trust for assume role"
+  type        = list(string)
+  default     = []
+}
+
+variable "cicd_role_name" {
+  description = "Name of the CICD role to create"
+  type        = string
+  default     = "cicd"
+}
+
+variable "cicd_role_description" {
+  description = "Description of the CICD role to create"
+  type        = string
+  default     = "Role assumed by CICD pipelines"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
